2196bdbf8991830379efb39a988edaea61527555
sikandrahere <148213515+sikandrahere@users.noreply.github.com>
2025-01-18 19:47:02 +0530

    logo update

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 function Logo({width = '100px'}) {
   return (
-    <div>Logo</div>
+    <div className='flex items-center'>
+      <img  width={width}src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/V-logo.svg/2048px-V-logo.svg.png" alt="" />
+      <p className='font-bold'>ViEw</p>
+    </div>
   )
 }
 
